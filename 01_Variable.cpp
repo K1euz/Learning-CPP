45d6710e26469b22ae753534824f480689419bf7
K1euz <nurasyltazhibaev007@gmail.com>
2026-02-16 09:34:38 +0500

    Varaible

---
+++ 01_Variable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+	int a, b;
+	std::cout << "Enter variable a: ";
+	std::cin >> a;
+
+	std::cout << "Enter variable b: ";
+	std::cin >> b;
+
+	std::cout << "A: " << a << ". B: " << b;
+
+	return 0;
+}
